cc52901bbcb80738f00c5a2a2db2e624fb4760ad
Zaki <menza.zm@gmail.com>
2023-05-04 23:06:51 +0600

    logo is setted

--- NamesCompApp/MainViewController.swift
+++ NamesCompApp/MainViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
-    
+final class MainViewController: UIViewController {
+    // MARK: - IB Outlets
     @IBOutlet var firstName: UITextField!
-    
     @IBOutlet var secondName: UITextField!
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,7 +24,18 @@ class MainViewController: UIViewController {
         resultVC.secondName = secondName.text
     }
     
+    // MARK: - IB Actions
     @IBAction func showResultButtonTapped() {
+        guard let firstNameTF = firstName.text, let secondNameTF = secondName.text else { return }
+        
+        if firstNameTF.isEmpty || secondNameTF.isEmpty {
+            displayAlert(
+                title: "Names are missing",
+                message: "Please enter both names"
+            )
+        
+        }
+        
         performSegue(withIdentifier: "goToResult", sender: nil)
     }
     
@@ -36,4 +46,31 @@ class MainViewController: UIViewController {
 
 
 }
-
+// MARK: - Alert Creating
+extension MainViewController {
+    private func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+// MARK: - TextField Delegate
+extension MainViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == firstName {
+            secondName.becomeFirstResponder()
+        } else {
+            showResultButtonTapped()
+        }
+        return true
+    }
+   
+    
+}
--- NamesCompApp/ResultViewController.swift
+++ NamesCompApp/ResultViewController.swift
@@ -7,26 +7,71 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
+final class ResultViewController: UIViewController {
     
-    
+    // MARK: - IB Outlets
     @IBOutlet var namesLabel: UILabel!
-    
     @IBOutlet var progressView: UIProgressView!
-    
     @IBOutlet var resultCalcLabel: UILabel!
     
+    // MARK: - Public properties
     var firstName: String!
     var secondName: String!
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
-
         
+        resultCalcLabel.text = getResult().formatted(.percent)
+        
+        progressView.progress = Float(getResult()) / 100
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 4)
+
     }
     
-
-   
-
+    // MARK: - Private methods
+    private func getValue(name: String) -> Int {
+        var count = 0
+        
+        switch name.lowercased() {
+        case "a", "b", "c", "q", "l":
+            count += 5
+        case "n", "r", "m":
+            count += 8
+        case "e", "t", "y":
+            count += 7
+        case "u", "i", "o":
+            count += 4
+        case "z", "x", "v":
+            count += 3
+        default:
+            count += 2
+        }
+        
+        return count
+    }
+    
+    private func getResult() -> Int {
+        var result = 0
+        
+        let firstValue = getValue(name: firstName)
+        let secondValue = getValue(name: secondName)
+        let difference = abs(firstValue - secondValue)
+        
+        switch difference {
+        case 1, 4, 6:
+            result = 100
+        case 3, 5, 7:
+            result = 65
+        case 2, 8, 9:
+            result = 85
+        case 10, 11, 12:
+            result = 45
+        default:
+            result = 30
+        }
+        
+        return result
+    }
 }
